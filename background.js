d17dc08fb473e0a30134cfe764e7867650906544
Carson Keller <carsonjkeller@gmail.com>
2023-02-15 13:10:35 -0700

    Working extension proof of concept

--- background.js
+++ background.js
@@ -1,26 +1,20 @@
-
+window.onload = function() {
+    document.getElementById("submitBtn").addEventListener('click', myFunction)
+}
 
 function myFunction() {
     var input = document.getElementById("searchTerm").value;
-    //document.getElementById("term").innerHTML = input
 
     req = new XMLHttpRequest;
 
-    req.open("GET",  `https://cast.boisestate.edu/test/splchk.php?word=${input}&max=5`, true);
+    req.open("GET", `https://cast.boisestate.edu/test/splchk.php?word=${input}&max=5`, true);
     req.send();
 
     req.onreadystatechange = function() {
         if(req.readyState == 4 && req.status == 200){
-            //console.log(req.responseText);
-
-            var result = req.responseText;
-            //console.log(typeof(result));
-            var JSONresult = JSON.parse(result);
-            var suggestions = JSONresult['suggestions']
-            //console.log(suggestions)
-            
-            //eof(JSONresult))
-            document.getElementById("results").innerHTML = suggestions.map(item => `<li>${item}</li>`);
+            var result = JSON.parse(req.responseText);
+            var suggestions = result['suggestions']
+            document.getElementById("results").innerHTML = suggestions.map(item => `<li>${item}</li>`).join('');
         }
     }
 };
